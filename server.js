1782b2e5de31152a92b40d3e3e6fc701798fcaff
netvision <rakesh@jangid.co.in>
2022-03-03 07:28:27 +0530

    updates new routes

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const axios = require('axios').default;
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3001;
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-  });
-app.get('/', (req, res) => {
-    const url = 'https://www1.nseindia.com/live_market/dynaContent/live_watch/stock_watch/liveIndexWatchData.json';
+app.use(cors())
+app.get('/indices', (req, res) => {
+    const url = 'https://www1.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json';
+    axios.get(url).then(r =>{
+        res.json(r.data);
+    })
+});
+app.get('/gainers', (req, res) => {
+    const url = 'https://www1.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json';
+    axios.get(url).then(r =>{
+        res.json(r.data);
+    })
+});
+app.get('/loosers', (req, res) => {
+    const url = 'https://www1.nseindia.com/live_market/dynaContent/live_analysis/losers/niftyLosers1.json';
     axios.get(url).then(r =>{
         res.json(r.data);
     })
 });
 
-app.listen(port, () => console.log('server running'));
+app.get('/scrip/:id', (req, res) => {
+    const id = req.params.id;
+    const url = 'https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxGetQuoteJSON.jsp?series=EQ&symbol='+id;
+    axios.get(url).then(r =>{
+        res.json(r.data);
+    })
+});
+
+
+app.listen(port, () => console.log('server running on: '+port));
